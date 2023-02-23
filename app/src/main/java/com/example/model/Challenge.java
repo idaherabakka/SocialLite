1654fab0d9c00ac46c5aa8880c5186e891173cc8
Lisa Maria Eliassen <lisaeli@hotmail.no>
2023-02-23 19:16:14 +0100

    RecyclerView support for challenges in overview screen.

---
+++ app/src/main/java/com/example/model/Challenge.java
@@ -0,0 +1,45 @@
+package com.example.model;
+
+public class Challenge {
+    private String title;
+    private String creator;
+    private String deadline;
+    private String date_created;
+
+    public Challenge(String title, String creator) {
+        this.title = title;
+        this.creator = creator;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(String date_created) {
+        this.date_created = date_created;
+    }
+}
---
+++ app/src/main/java/com/example/model/ViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sociallite.R;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    TextView titleView;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        titleView = itemView.findViewById(R.id.title);
+    }
+}
