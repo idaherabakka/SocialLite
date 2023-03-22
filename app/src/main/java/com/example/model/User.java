f0293b2b9af25d92fc4f52180711fd373e1aa72d
MariaBonde <mariabonde99@gmail.com>
2023-03-22 16:01:50 +0100

    Added relevant lists to challenge and user and points class

--- app/src/main/java/com/example/model/User.java
+++ app/src/main/java/com/example/model/User.java
@@ -1,19 +1,97 @@
 package com.example.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class User {
     public String firstname;
     public String lastname;
-    public int age;
     public String ID;
     private String password;
     private String email;
 
-    public User(String firstname, String lastname, int age, String ID, String password, String email) {
+    private ArrayList<String> createdChallenges;
+    private ArrayList<String> joinedChallenges;
+
+    public User(String firstname, String lastname, String ID, String password, String email) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this.age = age;
         this.ID = ID;
         this.password = password;
         this.email = email;
     }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getCreatedChallenges() {
+        return createdChallenges;
+    }
+
+    public void setCreatedChallenges(ArrayList<String> createdChallenges) {
+        this.createdChallenges = createdChallenges;
+    }
+
+    public ArrayList<String> getJoinedChallenges() {
+        return joinedChallenges;
+    }
+
+    public void setJoinedChallenges(ArrayList<String> joinedChallenges) {
+        this.joinedChallenges = joinedChallenges;
+    }
+
+    public void addJoined(String challengeId){
+        joinedChallenges.add(challengeId);
+    }
+
+    public void removeJoined(String challengeId) {
+        joinedChallenges.remove(challengeId);
+    }
+
+    public void addCreated(String challengeId){
+        joinedChallenges.add(challengeId);
+    }
+
+    public void removeCreated(String challengeId) {
+        joinedChallenges.remove(challengeId);
+    }
+
+
 }
