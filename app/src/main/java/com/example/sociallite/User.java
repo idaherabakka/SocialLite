7d0cefcd25c441cfba4efb801983d1e744693cc5
Thumsificial <92872386+Thumsificial@users.noreply.github.com>
2023-03-22 14:45:47 +0100

    Some initial changes to user, to fetch information from database.

--- app/src/main/java/com/example/sociallite/User.java
+++ app/src/main/java/com/example/sociallite/User.java
@@ -1,19 +1,35 @@
 package com.example.sociallite;
 
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class User {
+
+    String username;
     public String firstname;
     public String lastname;
     public int age;
     public String ID;
+
     private String password;
+
     private String email;
 
-    public User(String firstname, String lastname, int age, String ID, String password, String email) {
+    FirebaseAuth mAuth;
+
+    public User() {
+        mAuth = FirebaseAuth.getInstance();
+        username = mAuth.getCurrentUser().getDisplayName();
+    }
+
+    public User(String firstname, String lastname, int Age, String ID) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.age = age;
         this.ID = ID;
-        this.password = password;
-        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
