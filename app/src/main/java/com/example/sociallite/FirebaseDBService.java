183e0ede7299a0616a4c0037906d4d506de18846
idahe <xek010@uib.no>
2023-03-22 16:04:31 +0100

    update

--- app/src/main/java/com/example/sociallite/FirebaseDBService.java
+++ app/src/main/java/com/example/sociallite/FirebaseDBService.java
@@ -4,17 +4,15 @@ import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.model.Challenge;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -22,20 +20,45 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DatabaseActivity extends AppCompatActivity {
+public class FirebaseDBService extends AppCompatActivity {
     static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public FirebaseFirestore getDataBase(){
+        return db;
+    }
 
     public void DatabaseActivity(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
     }
 
-    //Add data
-    public void addChallenge(User us ) {
+    public void addUser(User u ) {
         Map<String, Object> user = new HashMap<>();
-        user.put("first", us.firstname);
-        user.put("last", us.lastname);
-        user.put("born", us.age);
+        user.put("first", u.getFirstname());
+        user.put("last", u.getLastname());
+        user.put("email", u.getEmail());
+
+        // Add a new document with a generated ID
+        db.collection("User").document(u.ID).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    //Add challenge
+    public void addChallenge(Challenge challengeObj) {
+        Map<String, Object> challenge = new HashMap<>();
+        challenge.put("title", challengeObj.getTitle());
+        challenge.put("creator", challengeObj.getCreator());
+        challenge.put("deadline", challengeObj.getDeadline());
+        challenge.put("created", challengeObj.getDate_created());
 
         // Add a new document with a generated ID
         db.collection("User").document(us.ID).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
