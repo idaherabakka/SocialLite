688b6b8bfd23604f2bd195206f20edb03efa9846
danielnyvoll <danielnyvoll@hotmail.com>
2023-03-28 12:47:22 +0200

    forgot password test

--- app/src/androidTest/java/com/example/sociallite/EspressoTest/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sociallite/EspressoTest/MainActivityTest.java
@@ -55,11 +55,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
