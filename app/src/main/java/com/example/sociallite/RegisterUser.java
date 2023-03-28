88ec93117d4b07f00b1e9f1db5182a952e7688f7
danielnyvoll <danielnyvoll@hotmail.com>
2023-03-28 14:22:10 +0200

    Minor fix in bug

--- app/src/main/java/com/example/sociallite/RegisterUser.java
+++ app/src/main/java/com/example/sociallite/RegisterUser.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.model.User;
+import com.example.service.FirebaseDBService;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
