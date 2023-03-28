40759bc4060b648abcd80ddf6fe800adf4a0c4a2
idahe <xek010@uib.no>
2023-03-28 18:21:44 +0200

    Base/draft frontend create challenge

--- app/src/main/java/com/example/sociallite/ChallengeActivity.java
+++ app/src/main/java/com/example/sociallite/ChallengeActivity.java
@@ -1,6 +1,8 @@
 package com.example.sociallite;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,18 @@ public class ChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createchallenge);
+
+
+        /*Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //spinner.setPrompt("Select your favorite Planet!");
+
+        spinner.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapter,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                        this));*/
     }
 }
