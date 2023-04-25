3e54d861a4d234462d4891126e6cf99b0a217aff
Thumsificial <92872386+Thumsificial@users.noreply.github.com>
2023-04-25 13:21:30 +0200

    Fixed all the stuff i did earlier

--- app/src/main/java/com/example/sociallite/EditProfileActivity.java
+++ app/src/main/java/com/example/sociallite/EditProfileActivity.java
@@ -2,20 +2,60 @@ package com.example.sociallite;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.model.User;
+import com.example.service.FirebaseDBService;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    FirebaseDBService db = new FirebaseDBService();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
+
+
+        String email = mAuth.getCurrentUser().getEmail();
+        User currentUser = db.getUser(mAuth.getCurrentUser().getEmail());
+        String firstname = currentUser.getFirstname();
+        String lastname = currentUser.getLastname();
+
+        EditText editFirstName = findViewById(R.id.editFirstName);
+        EditText editLastName = findViewById(R.id.editLastName);
+
+        editFirstName.setText(firstname);
+        editLastName.setText(lastname);
+
+
+        Button confirm = findViewById(R.id.confirm);
+
+        confirm.setOnClickListener(view -> {
+            if (TextUtils.isEmpty(editFirstName.getText().toString())) {
+                editFirstName.setError("First Name cannot be empty");
+                editFirstName.requestFocus();
+            }
+            if (TextUtils.isEmpty(editLastName.getText().toString())){
+                editLastName.setError("Last Name cannot be empty");
+                editLastName.requestFocus();
+            }
+            else {
+                currentUser.setFirstname(editFirstName.getText().toString());
+                currentUser.setLastname(editLastName.getText().toString());
+                db.updateUser(currentUser);
+                startActivity(new Intent(EditProfileActivity.this,MyProfileActivity.class));
+            }
+        });
 
         Button profileButton = findViewById(R.id.cancel);
         profileButton.setOnClickListener(view -> {
--- app/src/main/java/com/example/sociallite/MyProfileActivity.java
+++ app/src/main/java/com/example/sociallite/MyProfileActivity.java
@@ -8,60 +8,35 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.example.model.User;
+import com.example.service.FirebaseDBService;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    FirebaseDBService db = new FirebaseDBService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        String userEmail = mAuth.getCurrentUser().getEmail();
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        String userEmail = mAuth.getCurrentUser().getEmail();
+        String email = mAuth.getCurrentUser().getEmail();
+        User currentUser = db.getUser(mAuth.getCurrentUser().getEmail());
+        String firstname = currentUser.getFirstname();
+        String lastname = currentUser.getLastname();
 
-        TextView nameText = findViewById(R.id.name);
-        nameText.setText("Firstname Lastname");
-        String currentUser = mAuth.getCurrentUser().getEmail();
-        User = db.getUser(mAuth.getCurrentUser().getEmail());
 
         TextView nameText = findViewById(R.id.name);
-        nameText.setText("Firstname Lastname");
+        nameText.setText(firstname + " " + lastname);
 
         TextView emailText = findViewById(R.id.email);
-        emailText.setText(userEmail);
+        emailText.setText(email);
 
-        TextView emailText = findViewById(R.id.email);
-        emailText.setText(userEmail);
 
-        TextView email = findViewById(R.id.email);
-        email.setText(currentUser);
-        EditText firstName = findViewById(R.id.firstName);
-        EditText lastName = findViewById(R.id.lastName);
-        if(User.getFirstname() != null && User.getLastname()!= null){
-            firstName.setText(User.getFirstname());
-            lastName.setText(User.getLastname());
-        }
-        Button updateProfile = findViewById(R.id.updateProfile);
-        updateProfile.setOnClickListener(view -> {
-            if (TextUtils.isEmpty(firstName.getText().toString())) {
-                this.firstName.setError("First Name cannot be empty");
-                this.firstName.requestFocus();
-            }
-            if (TextUtils.isEmpty(lastName.getText().toString())){
-                this.lastName.setError("Last Name cannot be empty");
-                this.lastName.requestFocus();
-            }
-            else {
-                User.firstname = firstName.getText().toString();
-                User.lastname = lastName.getText().toString();
-                db.updateUser(User);
-                startActivity(new Intent(MyProfileActivity.this,OverviewActivity.class));
-            }
-        });
         Button profileButton = findViewById(R.id.back);
         profileButton.setOnClickListener(view -> {
             startActivity(new Intent(MyProfileActivity.this,OverviewActivity.class));;
